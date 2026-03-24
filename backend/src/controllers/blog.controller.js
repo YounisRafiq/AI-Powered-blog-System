0232b2e333961874b7c591e14134332d25c6c521
Younis <ya945055@gmail.com>
2026-03-24 15:50:27 +0500

    feat: implement getSingleBlog, deleteBlog, and updateBlog functionalities with proper error handling

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -23,10 +23,9 @@ const generateBlog = async (req, res) => {
   }
 };
 
-const getAllBlogs = async () => {
+const getAllBlogs = async (req , res) => {
    try {
-    const blogs = await blogModel.find().populate("author" , "fullName" , "email");
- 
+    const blogs = await blogModel.find();
     res.status(200).json({
      success : true,
      totalBlogs : blogs.length,
@@ -38,6 +37,89 @@ const getAllBlogs = async () => {
       message : error.message
     })
    }
+};
+
+const getSingleBlog = async (req , res) => {
+   const singleBlog = await blogModel.findById(req.params.id);
+
+   try {
+    if(!singleBlog){
+     return res.status(404).json({
+       message : "Error while fetching single blog"
+     })
+    };
+ 
+    res.status(200).json({
+     message : "Blog Fetched SuccessFully",
+     singleBlog
+    })
+   } catch (error) {
+    res.status(500).json({
+      error : error.message
+    })
+   }
 }
 
-module.exports = { generateBlog , getAllBlogs }
+const deleteBlog = async (req , res) => {
+  const blog = await blogModel.findById(req.params.id);
+
+  try {
+    if(!blog){
+      return res.status(401).json({
+        success : false,
+        message : "Blog NOT found"
+      })
+    };
+
+  if(blog.author.toString() !== req.user.id){
+    return res.status(403).json({ message : "NOT Authorized" })
+  };
+
+  const deletedBlog = await blogModel.deleteOne();
+
+  res.status(201).json({
+    message : "Blog Deleted SuccessFully",
+    deletedBlog
+  })
+
+  } catch (error) {
+    return res.status(500).json({
+      message : "Error happen while deleting the blog",
+      error : error.message
+    })
+  }
+};
+
+const updateBlog = async (req ,res) => {
+   const { title , content } = req.body;
+
+   const blog = await blogModel.findById(req.params.id);
+
+   try {
+    if(!blog){
+     return res.status(401).json({
+       message : "Blog NOT found"
+     })
+    };
+ 
+    if(blog.author.toString() !== req.user.id){
+     return res.status(403).json({ message : "NOT Authorized" })
+    };
+ 
+    blog.title = title || blog.title;
+    blog.content = content || blog.content;
+ 
+    await blog.save();
+ 
+    res.status(201).json({
+     message : "Blog Updated SuccessFully",
+     data : blog
+    });
+   } catch (error) {
+    res.status(500).json({
+      message : "Error happen while updating the blog",
+      error : error.message
+    })
+   }
+}
+module.exports = { generateBlog , getAllBlogs , getSingleBlog , deleteBlog , updateBlog }
--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -4,5 +4,9 @@ const blogController = require("../controllers/blog.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/generate" , authMiddleware.authMiddleware , blogController.generateBlog);
-router.get("/blogs", blogController.getAllBlogs)
+router.get("/allblogs" , blogController.getAllBlogs);
+router.get("/:id" , blogController.getSingleBlog);
+router.get("/delete/:id" , authMiddleware.authMiddleware ,  blogController.deleteBlog);
+router.put("/update/:id" , authMiddleware.authMiddleware , blogController.updateBlog);
+
 module.exports = router;
