077d68b848be8ddb3457b8470db666a3769edb19
Younis <ya945055@gmail.com>
2026-04-05 16:23:02 +0500

    Enhance layout with responsive sidebar and navbar, add Font Awesome for icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import Navbar from './components/Navbar';
-import './App.css';
-import Sidebar from './components/Sidebar';
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <>
-      <Navbar />
-      <Sidebar/>
-    </>
+    <div className={isOpen ? "layout open" : "layout closed"}>
+      
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+      
+      <div className="main-area">
+        <Navbar />
+      </div>
+
+    </div>
   );
 }
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import "./Sidebar.css"
-import logo from '../assets/image.png'
-const Sidebar = () => {
+import logo from "../assets/image.png"
+
+const Sidebar = ({ isOpen, setIsOpen }) => {
   return (
-    <div>
-      <div className="sidebar">
-         <div className="sidebar-logo">
-          <img src={logo} alt="This is logo" />
-         </div>
+    <div className={isOpen ? "sidebar" : "sidebar sidebar-closed"}>
+      
+      <div className="sidebar-logo">
+        
+        <img src={logo} onClick={() => setIsOpen(true)} />
+
+        <i 
+          className="fa-solid fa-xmark" style={{opacity: isOpen ? 1 : 0}}
+          onClick={() => setIsOpen(false)}
+        ></i>
+
       </div>
+
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar
-
