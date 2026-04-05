44baf24852bac2509b3507399083ea6975b178c3
Younis <ya945055@gmail.com>
2026-04-05 11:17:16 +0500

    add Navbar component with styling and logo integration

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import "./Navbar.css"
+import image from "../assets/image.png"
 const Navbar = () => {
   return (
     <div>
-      
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img src={image} alt="This is brand Logo" />
+          <h2>Blogi</h2>
+        </div>
+      </nav>
     </div>
   )
 }
